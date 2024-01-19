e9cdd331b73c6c5889953d26367c08a58faf0451
Uladimir Hrydzin <hrydzin@gmail.com>
2024-01-19 19:51:34 +0400

    refactor: create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -2,7 +2,7 @@
 import { createRequire } from "module";
 const require = createRequire(import.meta.url);
 
-const fs = require('fs');
+const fs = require('fs').promises;
 const path = require('path');
 
 const create = async () => {
@@ -10,15 +10,17 @@ const create = async () => {
     const textContent = 'I am fresh and young';
     const filePath = path.join('files', 'fresh.txt');
 
-
-    if (fs.existsSync(filePath)) {
+    try {
+        await fs.access(filePath);
         throw new Error('FS operation failed');
+      } catch (error) {
+        try {
+          await fs.writeFile(filePath, textContent);
+          console.log('File created successfully:', filePath);
+        } catch (Error) {
+          console.error('Error creating file:', Error);
+        }
       }
-
-    fs.writeFileSync(filePath, textContent, (error) => {
-        if(error) throw error;
-        console.log('File created successfully:', filePath);
-    });
 };
 
 await create();
