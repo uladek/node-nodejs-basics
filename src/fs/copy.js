ce7df4fed7145b1da7bed1e795d33a87c133daa4
Uladimir Hrydzin <hrydzin@gmail.com>
2024-01-19 22:12:55 +0400

    feat: inplement function copy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,35 @@
+
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+const fs = require('fs').promises;
+
+const path = require('path');
+
 const copy = async () => {
-    // Write your code here 
+    const sourceFolder = 'files';
+    const copyFolder = 'files_copy';
+
+    try {
+        await fs.access(sourceFolder);
+        try {
+            await fs.access(copyFolder);
+            throw new Error('FS operation failed');
+        } catch (error) {
+            await fs.mkdir(copyFolder);
+
+            const files = await fs.readdir(sourceFolder);
+
+            await Promise.all(files.map(async (file) => {
+                const sourceFilePath = path.join(sourceFolder, file);
+                const copyFilePath = path.join(copyFolder, file);
+
+                await fs.copyFile(sourceFilePath, copyFilePath);
+            }));
+            console.log(`Folder copied successfully:  ${sourceFolder} to ${copyFolder}`);
+        }
+    } catch (e) {
+        console.error(e.message);
+    }
 };
 
 await copy();
