d9a3aec1f90c6e24ecfd5efe2324b532ad655e85
Uladimir Hrydzin <hrydzin@gmail.com>
2024-01-19 18:49:54 +0400

    feat: add implement function create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,24 @@
+
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+
+const fs = require('fs');
+const path = require('path');
+
 const create = async () => {
-    // Write your code here 
+
+    const textContent = 'I am fresh and young';
+    const filePath = path.join('files', 'fresh.txt');
+
+
+    if (fs.existsSync(filePath)) {
+        throw new Error('FS operation failed');
+      }
+
+    fs.writeFileSync(filePath, textContent, (error) => {
+        if(error) throw error;
+        console.log('File created successfully:', filePath);
+    });
 };
 
 await create();
