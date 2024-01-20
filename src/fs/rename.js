786d961c3307e52fb54f6e842e61a38a77336953
Uladimir Hrydzin <hrydzin@gmail.com>
2024-01-20 13:47:42 +0400

    feat: inplement function rename

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,24 @@
+import fs from 'fs/promises';
+import path from 'path';
+
 const rename = async () => {
-    // Write your code here 
+    const oldNameFile = path.join('files', 'wrongFilename.txt');
+    const newNameFile = path.join('files', 'properFilename.md');
+
+    try {
+        await fs.access(oldNameFile);
+
+        try {
+            await fs.access(newNameFile);
+
+            throw new Error('FS operation failed. newFileName already exists.');
+        } catch (error) {
+            await fs.rename(oldNameFile, newNameFile);
+            console.log(`File renamed successfully: ${oldNameFile} to ${newNameFile}`);
+        }
+    }  catch (e) {
+        console.error(e.message);
+    }
 };
 
 await rename();
