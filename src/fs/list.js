35c2c23f048cec4eae694f626464c9e94d5a30c3
Uladimir Hrydzin <hrydzin@gmail.com>
2024-01-20 16:13:38 +0400

    feat: inplement function list

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,20 @@
+
+import fs from 'fs/promises';
+import path from 'path';
+
 const list = async () => {
-    // Write your code here 
+
+    const folderPath = path.join('files');
+
+    try {
+        await fs.access(folderPath);
+
+        const fileNames = await fs.readdir(folderPath);
+        console.log('Filenames in the "files" folder:', fileNames);
+
+    } catch (error) {
+        throw new Error(`FS operation failed. Folder does not exist:: ${folderPath}`);
+    }
 };
 
 await list();
