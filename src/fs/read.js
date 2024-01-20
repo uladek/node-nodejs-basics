f95d384bb7f62ca7e70812ecfb274e4554f8c56a
Uladimir Hrydzin <hrydzin@gmail.com>
2024-01-20 16:41:00 +0400

    feat: inplement  function read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,19 @@
+import fs from 'fs/promises';
+import path from 'path';
+
 const read = async () => {
-    // Write your code here 
+
+    const fileToRead = path.join('files', 'fileToRead.txt');
+
+    try {
+        await fs.access(fileToRead);
+
+        const content = await fs.readFile(fileToRead, 'utf-8');
+        console.log(`Content of fileToRead.txt:\n${content}`);
+    } catch (error) {
+        throw new Error('FS operation failed. File does not exist: fileToRead.txt');
+    }
+
 };
 
 await read();
