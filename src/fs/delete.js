7e6977c38b66457180f21297940bdc436218031f
Uladimir Hrydzin <hrydzin@gmail.com>
2024-01-20 15:01:27 +0400

    feat: inplement function delete

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,17 @@
+import fs from 'fs/promises';
+import path from 'path';
+
+
 const remove = async () => {
-    // Write your code here 
+    const fileToRemove = path.join('files', 'fileToRemove.txt');
+    try {
+        await fs.access(fileToRemove);
+        throw new Error(`FS operation failed. File does not exist: ${fileToRemove}`);
+        } catch (error) {
+
+          await fs.unlink(fileToRemove);
+            console.log(`File deleted successfully: ${fileToRemove}`);
+    }
 };
 
 await remove();
