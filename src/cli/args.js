0e8584d56bf9fe7b7b42e4351596c1917c1d0628
Uladimir Hrydzin <hrydzin@gmail.com>
2024-01-21 20:49:17 +0400

    feat: impelement function parseArgs

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,11 @@
 const parseArgs = () => {
-    // Write your code here 
+    const args = process.argv;
+
+    for (let i = 0; i < args.length; i += 2) {
+        const propName = args[i].slice(2);
+        const value = args[i + 1];
+        console.log(`${propName} is ${value}`);
+      }
 };
 
 parseArgs();
