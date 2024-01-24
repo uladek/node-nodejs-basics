c287f1b027b513a13c85ca9cc45733f06d137860
Uladimir Hrydzin <hrydzin@gmail.com>
2024-01-24 16:00:56 +0400

    feat: add implement function  read in folder stream

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,24 @@
+import { createReadStream } from 'node:fs';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const fileToRead = join(__dirname, 'files/fileToRead.txt');
+
 const read = async () => {
-    // Write your code here 
+    const stream = createReadStream(fileToRead);
+
+    stream.on('data', (chunk) => {
+        process.stdout.write(chunk);
+    })
+
+    stream.on('end', () => {
+      process.stdout.write('\n');
+    });
+
+    stream.on('error', (error) => {
+      console.error(`Error reading file: ${error.message}`);
+    });
 };
 
 await read();
