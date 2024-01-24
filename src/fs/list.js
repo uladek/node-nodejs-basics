97a63017ea1638e297861adfe5799472138293b1
Uladimir Hrydzin <hrydzin@gmail.com>
2024-01-25 02:18:02 +0400

    refactor: list.js

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,19 +1,18 @@
+import { access, readdir } from "node:fs/promises";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
 
-import fs from 'fs/promises';
-import path from 'path';
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const folderPath = join(__dirname, 'files');
 
 const list = async () => {
-
-    const folderPath = path.join('files');
-
     try {
-        await fs.access(folderPath);
+        await access(folderPath);
 
-        const fileNames = await fs.readdir(folderPath);
+        const fileNames = await readdir(folderPath);
         console.log('Filenames in the "files" folder:', fileNames);
-
     } catch (error) {
-        throw new Error(`FS operation failed. Folder does not exist:: ${folderPath}`);
+        throw new Error(`FS operation failed. ${error.message}`);
     }
 };
 
