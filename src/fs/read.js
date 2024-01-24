e8e893c80b8a87828c0f550f18a755462cf490e0
Uladimir Hrydzin <hrydzin@gmail.com>
2024-01-25 02:56:07 +0400

    refactor: read.js in fs folder

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,19 +1,22 @@
-import fs from 'fs/promises';
-import path from 'path';
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+import { access, readFile } from 'node:fs/promises';
+
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const fileToRead = join(__dirname, 'files/fileToRead.txt');
+
 
 const read = async () => {
 
-    const fileToRead = path.join('files', 'fileToRead.txt');
-
     try {
-        await fs.access(fileToRead);
+        await access(fileToRead);
 
-        const content = await fs.readFile(fileToRead, 'utf-8');
+        const content = await readFile(fileToRead, 'utf-8');
         console.log(`Content of fileToRead.txt:\n${content}`);
     } catch (error) {
-        throw new Error('FS operation failed. File does not exist: fileToRead.txt');
+        throw new Error(`FS operation failed. File does not exist: ${fileToRead}`);
     }
-
 };
 
 await read();
