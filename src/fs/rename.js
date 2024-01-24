7625c06dbdfc3da5528c93b4961864d4b310d4ad
Uladimir Hrydzin <hrydzin@gmail.com>
2024-01-24 16:17:24 +0400

    refactor: rename.js file

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,19 +1,25 @@
-import fs from 'fs/promises';
+// import fs from 'fs/promises';
 import path from 'path';
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+import { access, rename as renameToNewName } from 'node:fs/promises';
+
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const oldNameFile = join(__dirname, 'files/wrongFilename.txt');
+const newNameFile  = join(__dirname, 'files/properFilename.md');
+
 
 const rename = async () => {
-    const oldNameFile = path.join('files', 'wrongFilename.txt');
-    const newNameFile = path.join('files', 'properFilename.md');
-
     try {
-        await fs.access(oldNameFile);
+        await access(oldNameFile);
 
         try {
-            await fs.access(newNameFile);
+            await access(newNameFile);
 
             throw new Error('FS operation failed. newFileName already exists.');
         } catch (error) {
-            await fs.rename(oldNameFile, newNameFile);
+            await renameToNewName(oldNameFile, newNameFile);
             console.log(`File renamed successfully: ${oldNameFile} to ${newNameFile}`);
         }
     }  catch (e) {
