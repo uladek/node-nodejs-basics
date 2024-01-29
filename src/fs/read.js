7acfdb6cbb1235ec43a256dabebfbe20c29a404a
Uladimir Hrydzin <hrydzin@gmail.com>
2024-01-30 00:52:27 +0400

    fix: function read in fs folder

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,21 +1,16 @@
-import { dirname, join } from "path";
-import { fileURLToPath } from "url";
-import { access, readFile } from 'node:fs/promises';
-
+import { readFile } from 'node:fs/promises';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const fileToRead = join(__dirname, 'files/fileToRead.txt');
 
-
 const read = async () => {
-
     try {
-        await access(fileToRead);
-
         const content = await readFile(fileToRead, 'utf-8');
         console.log(`Content of fileToRead.txt:\n${content}`);
     } catch (error) {
-        throw new Error(`FS operation failed. File does not exist: ${fileToRead}`);
+            throw new Error(`FS operation failed. File does not exist: ${fileToRead}`);
     }
 };
 
