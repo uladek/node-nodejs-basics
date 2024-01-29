f9c6ac6d7bd00979a2fc7862de2a5f35cc68d9a6
Uladimir Hrydzin <hrydzin@gmail.com>
2024-01-30 00:23:06 +0400

    fix: function remove

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,21 +1,30 @@
-import { unlink, access } from "node:fs/promises";
-import { dirname, join } from "path";
-import { fileURLToPath } from "url";
-
+import { unlink, rm } from 'node:fs/promises';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const fileToRemove = join(__dirname, 'files/fileToRemove.txt');
 
-
 const remove = async () => {
-    try {
-        await access(fileToRemove);
-        throw new Error(`FS operation failed. File does not exist: ${fileToRemove}`);
-        } catch (error) {
 
-          await unlink(fileToRemove);
-            console.log(`File deleted successfully: ${fileToRemove}`);
+    try {
+        await unlink(fileToRemove);
+        console.log(`File deleted successfully: ${fileToRemove}`);
+    } catch (error) {
+        if (error.code === 'ENOENT') {
+            throw new Error(`FS operation failed. File does not exist: ${fileToRemove}`);
+        } else {
+            console.error(error.message);
+        }
     }
+
+// but better way:
+//  try{
+//     await rm(fileToRemove)
+//     console.log(`File deleted successfully: ${fileToRemove}`);
+//  } catch {
+//     throw new Error(`FS operation failed. File does not exist: ${fileToRemove}`);
+//  }
 };
 
 await remove();
