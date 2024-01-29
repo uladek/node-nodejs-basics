1e2255bda38923130415bd0d39c353f5e930114a
Uladimir Hrydzin <hrydzin@gmail.com>
2024-01-29 23:53:47 +0400

    fix: rename function

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,27 +1,39 @@
-import { dirname, join } from "path";
-import { fileURLToPath } from "url";
-import { access, rename as renameToNewName } from 'node:fs/promises';
-
+import { rename as renameToNewName, stat } from 'node:fs/promises';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const oldNameFile = join(__dirname, 'files/wrongFilename.txt');
-const newNameFile  = join(__dirname, 'files/properFilename.md');
+const newNameFile = join(__dirname, 'files/properFilename.md');
 
+const fileExists = async (filePath) => {
+    try {
+        await stat(filePath);
+        return true;
+    } catch (error) {
+        if (error.code === 'ENOENT') {
+            return false;
+        } else {
+            throw new Error(' FS operation failed: Destination file already exists');
+            throw error;
+
+        }
+    }
+};
 
 const rename = async () => {
     try {
-        await access(oldNameFile);
-
-        try {
-            await access(newNameFile);
-
-            throw new Error('FS operation failed. newFileName already exists.');
-        } catch (error) {
-            await renameToNewName(oldNameFile, newNameFile);
-            console.log(`File renamed successfully: ${oldNameFile} to ${newNameFile}`);
+        if (await fileExists(newNameFile)) {
+            throw new Error(' FS operation failed: Destination file already exists');
         }
-    }  catch (e) {
-        console.error(e.message);
+        await renameToNewName(oldNameFile, newNameFile);
+        console.log(`File renamed successfully: ${oldNameFile} to ${newNameFile}`);
+    } catch (error) {
+        if (error.code === 'ENOENT') {
+            throw new Error('FS operation failed: Source file does not exist');
+        } else {
+            console.error(error.message);
+        }
     }
 };
 
