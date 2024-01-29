3a6b6ce3ee528e41bebb1585cbcce348ab18e478
Uladimir Hrydzin <hrydzin@gmail.com>
2024-01-30 02:13:19 +0400

    refactor: esm.mjs

--- src/cli/env.js
+++ src/cli/env.js
@@ -4,7 +4,7 @@ const parseEnv = () => {
     const prefix = 'RSS_';
 
     const envVKeys = process.env;
-    const rssA = Object.keys(envVKeys);
+    // const rssA = Object.keys(envVKeys);
 
     const rssVariables = Object.keys(envVKeys)
       .filter((key) => key.startsWith(prefix))
--- src/modules/esm.mjs
+++ src/modules/esm.mjs
@@ -10,27 +10,30 @@ import { release, version } from 'os';
 import { createServer as createServerHttp } from 'http';
 // require('./files/c');
 import './files/c.js';
-
+import  { createRequire } from 'mode:module';
+const require = createRequire(import.meta.url);
 
 const random = Math.random();
 
 let unknownObject;
 
 if (random > 0.5) {
+
     // unknownObject = require('./files/a.json');
-    // unknownObject = import('./files/a.json');
-    unknownObject = await import("./files/a.json", {
-        assert: { type: "json" },
-      });
+        unknownObject = require('./files/a.json');
 
-
+    // or? unknownObject = await import("./files/a.json", {
+    //     assert: { type: "json" },
+    //   });
 } else {
-    // unknownObject = require('./files/b.json');
-    // unknownObject = import('./files/b.json');
-    unknownObject = await import("./files/b.json", {
-        assert: { type: "json" },
-      });
 
+
+    // unknownObject = require('./files/a.json');
+        unknownObject = require('./files/a.json');
+
+    // unknownObject = await import("./files/b.json", {
+    //     assert: { type: "json" },
+    //   });
 }
 
 console.log(`Release ${release()}`);
@@ -38,12 +41,12 @@ console.log(`Version ${version()}`);
 console.log(`Path segment separator is "${sep}"`);
 
 // console.log(`Path to current file is ${__filename}`);
-const filenameESM = fileURLToPath(import.meta.url);
-console.log(`Path to current file is ${filenameESM}`);
+const __filenameESM = fileURLToPath(import.meta.url);
+console.log(`Path to current file is ${__filenameESM}`);
 
-const dirnameESM = dirname(filenameESM);
+const __dirnameESM = dirname(filenameESM);
 // console.log(`Path to current directory is ${__dirname}`);
-console.log(`Path to current directory is ${dirnameESM}`);
+console.log(`Path to current directory is ${__dirnameESM}`);
 
 
 const myServer = createServerHttp((_, res) => {
