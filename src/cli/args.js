cdcbaeb0c74c2c96c55f6502dadb9120501ee9f7
Uladimir Hrydzin <hrydzin@gmail.com>
2024-01-30 01:11:55 +0400

    fix: function parseArgs

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,11 +1,15 @@
 const parseArgs = () => {
-    const args = process.argv;
+  const args = process.argv;
+  let output = '';
 
-    for (let i = 0; i < args.length; i += 2) {
-        const propName = args[i].slice(2);
-        const value = args[i + 1];
-        console.log(`${propName} is ${value}`);
-      }
+  for (let i = 2; i < args.length; i += 2) {
+      const propName = args[i].slice(2);
+      const value = args[i + 1];
+      output += `${propName} is ${value}, `;
+  }
+
+    output = output.slice(0, -2);
+    console.log(output);
 };
 
 parseArgs();
